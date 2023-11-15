63dade4db34118836e90ceb23ec7fb1bb6be751d
Alyssa Simon <138164179+alyssasimons@users.noreply.github.com>
2023-11-15 00:33:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+const questions = [
+    {
+        question: "What is AudioLens?",
+        answers: [
+            { text: "A camera that has audio", correct: false },
+            { text: "AI/ML prescription glasses", correct: true },
+            { text: "A new phone that has enhanced audio", correct: false },
+        ]
+    },
+    {
+        question: "What audience is AudioLens catered to?",
+        answers: [
+            { text: "Those hard of hearing", correct: false },
+            { text: "Those that cannot walk", correct: false },
+            { text: "The visually impaired", correct: true },
+        ]
+    },
+    {
+        question: "What does AudioLens do?",
+        answers: [
+            { text: "It helps detect obstructions, recognize faces, read upon text, and more", correct: true },
+            { text: "It helps create better audio by enhancing its quality", correct: false },
+            { text: "It helps cameras zoom in further", correct: false },
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-btn");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ", " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    })
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again"
+    nextButton.style.display = "block";
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+startQuiz();
